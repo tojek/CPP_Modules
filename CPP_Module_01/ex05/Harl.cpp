128a8ec4d8ae41941734a6c8220ac6e491b601ac
mateusz <mateusz.wojtczak42@gmail.com>
2025-03-09 20:25:32 +0100

    01/05

---
+++ CPP_Module_01/ex05/Harl.cpp
@@ -0,0 +1,47 @@
+#include "Harl.hpp"
+
+Harl::Harl( void )
+{
+	std::cout << "Harl is coming!" << std::endl;
+}
+
+Harl::~Harl( void )
+{
+	std::cout << "Harl is leaving!" << std::endl;
+}
+
+void Harl::debug ( void )
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void Harl::info ( void )
+{
+	std::cout << "I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!" << std::endl;
+}
+
+void Harl::warning ( void )
+{
+	std::cout << "I’ve been coming here for years and you just started working here last month." << std::endl;
+}
+
+void Harl::error ( void )
+{
+	std::cout << "This is unacceptable. I want to speak to the manager now." << std::endl;
+}
+
+void Harl::complain( std::string level )
+{
+	void (Harl::*complaints[])( void ) = { &Harl::debug, &Harl::info, &Harl::warning, &Harl::error };
+	std::string levels[] = { "DEBUG", "INFO", "WARNING", "ERROR" };
+
+	for (int i = 0; i < 4; i++)
+	{
+		if (levels[i] == level)
+		{
+			(this->*complaints[i])();
+			return ;
+		}
+	}
+	std::cout << "Invalid complaint level" << std::endl;
+}
