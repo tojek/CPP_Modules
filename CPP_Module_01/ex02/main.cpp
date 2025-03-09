44067c2293af8fddbe908c0563d9f08513aacf19
mateusz <mateusz.wojtczak42@gmail.com>
2025-03-09 17:17:24 +0100

    01/02 & 03

---
+++ CPP_Module_01/ex02/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string string = "HI THIS IS BRAIN";
+    std::string* stringPTR = &string;
+    std::string& stringREF = string;
+
+    // Print memory addresses
+    std::cout << "Memory addresses:" << std::endl;
+    std::cout << "string:    " << &string << std::endl;
+    std::cout << "stringPTR: " << stringPTR << std::endl;
+    std::cout << "stringREF: " << &stringREF << std::endl;
+
+    // Print values
+    std::cout << "\nValues:" << std::endl;
+    std::cout << "string:    " << string << std::endl;
+    std::cout << "stringPTR: " << *stringPTR << std::endl;
+    std::cout << "stringREF: " << stringREF << std::endl;
+
+    return 0;
+}
---
+++ CPP_Module_01/ex03/HumanB.cpp
@@ -0,0 +1,22 @@
+#include "HumanB.hpp"
+
+HumanB::HumanB(std::string name) : name(name)
+{
+}
+
+HumanB::~HumanB()
+{
+}
+
+void HumanB::attack()
+{
+	if (this->weapon)
+		std::cout << this->name << " attacks with his " << this->weapon->getType() << std::endl;
+	else
+		std::cout << this->name << " has no weapon" << std::endl;
+}
+
+void HumanB::setWeapon(Weapon &weapon)
+{
+	this->weapon = &weapon;
+}
