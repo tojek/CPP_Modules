c7f827d4775d3d1f28381b65a3a97d762ee221a9
mateusz <mateusz.wojtczak42@gmail.com>
2025-10-22 18:18:34 +0200

    start cpp07

---
+++ CPP_Module_07/ex00/Whatever.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+template<typename T>
+void swap(T& a, T& b)
+{
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+template<typename T>
+T min(T const& a, T const& b)
+{
+    return (a < b) ? a : b;
+}
+
+template<typename T>
+T max(T const& a, T const& b)
+{
+    return (a > b) ? a : b;
+}
